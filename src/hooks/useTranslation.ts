cfb5aca5b430afc47dfeb7ff079cb678a0b05fda
ABDE777 <mazgouraabdalmounim@gmail.com>
2025-10-23 11:38:16 +0100

    last one upadates

--- src/hooks/useTranslation.ts
+++
@@ -1,7 +0,0 @@
-import { useLanguage } from "@/contexts/LanguageContext";
-
-// This hook provides a simple way to access translations
-export const useTranslation = () => {
-  const { t } = useLanguage();
-  return { t };
-};
--- supabase/migrations/20251020002115_83a7f800-5ca7-4e3b-bc49-2b0793dde9e3.sql
+++
@@ -1,3 +0,0 @@
--- Add video_completed column to players table
-ALTER TABLE public.players 
-ADD COLUMN IF NOT EXISTS video_completed boolean DEFAULT false;
